624aa19b5dd3dde4c767b9bb040a8e088f08dff7
ririyad <rasheduliriyad@gmail.com>
2021-12-04 11:29:15 +0600

    Updated code and README

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Maincontent from "./components/Main"
+import "./style.css"
 
 export default function App() {
     return (
-        <div>
+        <div className="container">
             <Navbar />
             <Maincontent />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
+import reactlogo from "../images/react-icon-small.png"
 
 export default function Navbar() {
     return(
-        <h1>This is the navbar section</h1>
+        <nav>
+            <img 
+                src= {reactlogo}
+                alt="React Logo"
+                className="nav-icon" 
+            />
+            <h2 className="nav-logo-text">ReactFacts</h2>
+            <h3 className="nav-title">React Course: Project 01 </h3>
+        </nav>
     )
 }
