c742279abf827109658f74119dd17a4a878adfb9
ririyad <rasheduliriyad@gmail.com>
2021-12-04 12:04:18 +0600

    Updating styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ export default function Navbar() {
                 alt="React Logo"
                 className="nav-icon" 
             />
-            <h2 className="nav-logo-text">ReactFacts</h2>
-            <h3 className="nav-title">React Course: Project 01 </h3>
+            <h3 className="nav-logo-text">ReactFacts</h3>
+            <h4 className="nav-title">React Course: Project 01 </h4>
         </nav>
     )
 }
