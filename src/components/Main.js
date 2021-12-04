7594205f09028e86a79f4bb149a4925e72327c8f
ririyad <rasheduliriyad@gmail.com>
2021-12-04 13:45:35 +0600

    Updated styles and content

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
+//import mainSectionImage from "../images/react-icon-large.png"
 
 export default function Main() {
     return (
-        <h1>Main Section </h1>
+        <main>
+                <h2 className="main-title">Facts about React</h2>
+                <ul className="main-facts">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Is Maintained by Facebook</li>
+                </ul>
+
+        </main>
     )
 }
